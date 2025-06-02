0d827c5196598926ff4cfe0d1a56b7b7fb685110
aaron050223 <agalcan1410@g.educaand.es>
2025-06-02 19:20:09 +0200

    Añado el HTML, CSS y JS (Inicio)

---
+++ js/carrusel.js
@@ -0,0 +1,16 @@
+const imagenes = document.querySelectorAll('.carrusel-imagenes img');
+const prevBtn = document.querySelector('.carrusel-btn.prev');
+const nextBtn = document.querySelector('.carrusel-btn.next');
+let indice = 0;
+
+function mostrarImagen(indiceNuevo) {
+    imagenes[indice].classList.remove('activo');
+    indice = (indiceNuevo + imagenes.length) % imagenes.length;
+    imagenes[indice].classList.add('activo');
+}
+
+prevBtn.addEventListener('click', () => mostrarImagen(indice - 1));
+nextBtn.addEventListener('click', () => mostrarImagen(indice + 1));
+
+// Opcional: Carrusel automático
+setInterval(() => mostrarImagen(indice + 1), 4000);
